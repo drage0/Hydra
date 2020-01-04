2caaba906e550073168b43a1ebfe76981ea0ed2a
drage0 <alek.sandar0@yandex.com>
2020-01-04 18:55:04 +0100

    Documentation and more style defines.

--- src/main.c
+++ src/main.c
@@ -33,6 +33,8 @@ helpme(char *program)
 
 /*
  * Return pointer of the first non-whitespace character in the given string.
+ * Example: str="  lzd", trim(str) would return str+2, since the first
+ * non-whitespace character is 'l', offset of 2.
  */
 static char *
 trim(char *str)
@@ -45,17 +47,25 @@ trim(char *str)
 	return trimmed;
 }
 
+/*
+ * Define the data used to make the HTML files.
+ * PAGE_WIDTH defines the maximum width of the line.
+ * TEXT_COLOUR defines the colour of the text.
+ */
 #define PAGE_WIDTH "42em"
+#define TEXT_COLOUR "#fff2da"
+#define LINK_COLOUR "#fa4040"
+#define LINK_VISITCOLOUR "#aa2040"
 static char *style =
 "html{background:url(\"res/qiron_seam.jpg\");background-color:#0d0c0c;background-repeat:repeat;font-family:\"Linux Libertine O\", \"Liberation Serif\", \"Times New Roman\", Times, serif;}\n\
-body{margin:2em auto;max-width:"PAGE_WIDTH";line-height:1.6;font-size:1.2em;color:#fff2da;padding:0 10px;image-rendering:pixelated;image-rendering:crisp-edges}\
+body{margin:2em auto;max-width:"PAGE_WIDTH";line-height:1.6;font-size:1.2em;color:"TEXT_COLOUR";padding:0 10px;image-rendering:pixelated;image-rendering:crisp-edges}\
 h1, h2{line-height:1.2; text-shadow: 2px 3px 0 #111}\n\
 h1{font-size:2.5em}\n\
 h2{font-size:2em;padding-top:1em}\n\
 h3{padding-right: 1em; text-shadow: 1px 2px 0 #111}\n\
 hr{margin-top: 2em; margin-bottom: 2em;border-color:#cfc2aa;color:#cfc2aa}\n\
-a{color:#fa4040}\n\
-a:visited{color:#aa2040}";
+a{color:"LINK_COLOUR"}\n\
+a:visited{color:"LINK_VISITCOLOUR"}";
 static char *backgroundcolour = "#0d0c0c";
 static char *keywords         = "Hydra,Software";
 static char *author           = "Aleksandar";
