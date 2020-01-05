f320ed384dc299bb9d79eb383af5f93397346248
drage0 <alek.sandar0@yandex.com>
2020-01-05 03:37:06 +0100

    Change styling.

--- src/main.c
+++ src/main.c
@@ -56,8 +56,9 @@ trim(char *str)
 #define TEXT_COLOUR "#fff2da"
 #define LINK_COLOUR "#fa4040"
 #define LINK_VISITCOLOUR "#aa2040"
+#define BACKGROUND_COLOUR "#353C51"
 static char *style =
-"html{background:url(\"res/qiron_seam.jpg\");background-color:#0d0c0c;background-repeat:repeat;font-family:\"Linux Libertine O\", \"Liberation Serif\", \"Times New Roman\", Times, serif;}\n\
+"html{background:url(\"res/qiron_seam.jpg\");background-color:"BACKGROUND_COLOUR";background-repeat:repeat;font-family:\"Linux Libertine O\", \"Liberation Serif\", \"Times New Roman\", Times, serif;}\n\
 body{margin:2em auto;max-width:"PAGE_WIDTH";line-height:1.6;font-size:1.2em;color:"TEXT_COLOUR";padding:0 10px;image-rendering:pixelated;image-rendering:crisp-edges}\
 h1, h2{line-height:1.2; text-shadow: 2px 3px 0 #111}\n\
 h1{font-size:2.5em}\n\
@@ -67,7 +68,6 @@ hr{margin-top: 2em; margin-bottom: 2em;border-color:#cfc2aa;color:#cfc2aa}\n\
 img{width:100%;height:auto}\n\
 a{color:"LINK_COLOUR"}\n\
 a:visited{color:"LINK_VISITCOLOUR"}";
-static char *backgroundcolour = "#0d0c0c";
 static char *keywords         = "Hydra,Software";
 static char *author           = "Aleksandar";
 static char *description      = "(denote void)";
@@ -96,7 +96,7 @@ convert(const char *path)
 	fputs("<head>\n", out);
 	fputs("<meta charset=\"UTF-8\">\n", out);
 	fputs("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n", out);
-	fprintf(out, "<meta name=\"theme-color\" content=\"%s\">\n", backgroundcolour);
+	fprintf(out, "<meta name=\"theme-color\" content=\"%s\">\n", BACKGROUND_COLOUR);
 	fprintf(out, "<meta name=\"keywords\" content=\"%s\">\n", keywords);
 	fprintf(out, "<meta name=\"author\" content=\"%s\">\n", author);
 	fprintf(out, "<meta name=\"description\" content=\"%s\">\n", description);
